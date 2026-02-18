98f0d8aeac41afb5e4ce1bbfff1044d4e8d7f7c3
sanchit <khuranasanchit07@gmail.com>
2026-02-18 14:22:52 +0530

    Add files via upload

---
+++ q18.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+    k = k % n;
+
+    int temp[n];
+    for (int i = 0; i < n; i++) {
+        temp[(i + k) % n] = arr[i];
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", temp[i]);
+    }
+
+    return 0;
+}
